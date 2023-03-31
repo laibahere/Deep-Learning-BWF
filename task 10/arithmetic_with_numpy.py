e94d559352a41722ce3bd8cb203880223fc885f7
laibahere <110159691+laibahere@users.noreply.github.com>
2023-03-31 17:04:44 +0500

    Add files via upload

---
+++ task 10/arithmetic_with_numpy.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+a= np.array([1,2,3,4,5])
+print(a)
+print(a+2)
+print(a-2)
+print(a*2)
+print(a/2)
+print(a**2) 
+c=np.sin(a)
+print(c)
+
+b=np.array([6,7,8,9,10])
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
