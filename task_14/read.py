7b9063f5c64977743d485a4603506bcfb01acfb9
laibahere <110159691+laibahere@users.noreply.github.com>
2023-04-02 20:03:55 +0500

    Update read.py

--- task_14/read.py
+++ task_14/read.py
@@ -13,3 +13,24 @@ df_xlsx= pd.read_excel('pokemon_data.xlsx')
 #to read txt file
 df_txt=pd.read_csv('pokemon_data.txt')
 print(df_txt.head(10))
+
+#-------------READING DATA IN PANDAS ---------
+#read headers
+print(df.columns)
+## read an specific columns
+print(df['Name'][0:5])  ##top five names
+print(df[['Name','HP']]) ##3 different columns
+#read each row
+print(df.head(4))
+print(df.iloc[1]) #for specific row 
+# for index,row in df.iterrows():
+     #print(index,rows['Name'])
+df.loc[df['Attack']== '50']     
+
+print(df.iloc[2:1]) #read a specific location
+
+#---------Sort/Describe Data---------
+xxx= df.describe()
+print(xxx)
+xo= df.sort_values(['Name','Generation'],ascending=False)
+print(xo)
