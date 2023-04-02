49ddbf196f71be2e216acd79984930c2bff160eb
laibahere <110159691+laibahere@users.noreply.github.com>
2023-04-02 19:32:46 +0500

    Update read.py

--- task_14/read.py
+++ task_14/read.py
@@ -1,1 +1,15 @@
 
+import pandas as pd
+#to read csv file
+df = pd.read_csv('pokemon_data.csv')  #to read csv files
+
+#print(df.head(4)) \\for top four rows
+#print(df.tail(3)) \\ for bottom 3 rows
+
+#to read excel file
+df_xlsx= pd.read_excel('pokemon_data.xlsx')
+#print(df_xlsx.head(3))
+
+#to read txt file
+df_txt=pd.read_csv('pokemon_data.txt')
+print(df_txt.head(10))
